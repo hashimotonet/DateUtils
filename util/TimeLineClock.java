0a26da7d7601b4b88106430fa88e4a1a436f4c3e
Osamu Hashimoto <osamu@hashimoto-net.jp>
2013-07-21 23:17:41 +0900

    compatible with Japanese Gengo(Added Implementation).

--- util/TimeLineClock.java
+++ util/TimeLineClock.java
@@ -8,16 +8,38 @@ import java.util.Date;
  * @version 0.0.8
  */
 
-// This class is under the test phase.
 public class TimeLineClock {
 
 	private final String STANDARD_TIME_FORMAT="YYYY/MM/dd HH:mm";
 	private final SimpleDateFormat sdf = new SimpleDateFormat(STANDARD_TIME_FORMAT);
+		
+	private final String CONVERT_TIME_FORMAT = "YYYY年MM月dd日";
+	private final SimpleDateFormat convFormat = new SimpleDateFormat(CONVERT_TIME_FORMAT);
 	
+	private final static String separater = "-----------------------------";
+
+	/**
+	 * 
+	 * @param date　指定日付
+	 * @return　フォーマットされた年月日
+	 */
 	public String getFormmatedDateAndTime(Date date) {
 		return sdf.format(date).toString();
 	}
 	
+	/** 
+	 * クラス内部で必要となった、フォーマットにてStringを返却する
+	 * @param Date
+	 * @return フォーマット化された日付文字列
+	 */
+	public String convertTimeFormat(Date date) {
+		return convFormat.format(date).toString();
+	}
+	
+	/**
+	 * 現在時刻を無条件に返却する。
+	 * @return　フォーマット化された年月日時刻
+	 */
 	public static synchronized String getCurrentDateAndTime() {
 		TimeLineClock clock = new TimeLineClock();
 		String time = clock.getFormmatedDateAndTime(new Date());
@@ -25,16 +47,57 @@ public class TimeLineClock {
 		return time;
 	}
 	
-	// debug.
+	/**
+	 * 
+	 * @return Formatted Japanese Date String.
+	 */
+	public static String getCurrentJapaneseDateAndTime() {
+		return getJapaneseDateAndTime(new Date());
+	}
+	
+	/**
+	 * 
+	 * @TODO Implementation.
+	 * @return Formatted Japanese Date String.
+	 */
+	public static synchronized String getJapaneseDateAndTime(Date date) {
+	    
+		Locale.setDefault(new Locale("ja","JP","JP"));
+        SimpleDateFormat fmt =  new SimpleDateFormat("GGGG yyyy'年'MM'月'dd'日' EEEE");
+        
+        //フォーマットを指定してSimpleDateFormatクラスのオブジェクトを生成
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yy'年'MM'月'dd'日'"); 
+
+        
+        //変換する文字列を生成 
+        Date date1=null;
+        String str1 = new String(new TimeLineClock().convertTimeFormat(date)); 
+        try { 
+           date1 = sdf1.parse(str1); //parseメソッドでDate型に変換
+            //変換できない場合例外がスローされます。
+        } catch (ParseException e) { 
+                System.out.println("変換できないフォーマットです。"); 
+        }
+        
+        String time1 = fmt.format(date);
+        String time2 = fmt.format(date1);
+        
+	    return time1;
+	}
+
 	public static void main(String args[]) {
 		
-		TimeLineClock clock = new TimeLineClock();
-		System.out.println(clock.getCurrentDateAndTime());
-		System.gc();
-		System.out.println(new TimeLineClock().getCurrentDateAndTime());
+		System.out.println(separater);
 		System.out.println(TimeLineClock.getCurrentDateAndTime());
+		System.out.println("Usage : call Class Method shown below.\nTimeLineClock.getCurrentDateAndTime() \nmethod from your source.\n");
 		
-		System.out.println("Usage : call Class Method shown below.\nTimeLineClock.getCurrentDateAndTime() \nmethod from your source.");
+		System.out.println(separater);
+		System.out.println(TimeLineClock.getJapaneseDateAndTime(new Date()));
+		System.out.println("Usage : call Class Method shown below.\nTimeLineClock.getJapaneseDateAndTime(java.util.Date) \nmethod from your source.\n");
 		
+		System.out.println(separater);
+		System.out.println(TimeLineClock.getCurrentJapaneseDateAndTime());
+		System.out.println("Usage : call Class Method shown below.\nTimeLineClock.getCurrentJapaneseDateAndTime() \nmethod from your source.\n");
+		System.out.println(separater);
 	}
 }
